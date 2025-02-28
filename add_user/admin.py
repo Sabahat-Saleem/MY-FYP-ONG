527ffa6b9fdf3f5b9a5b00d978e5db75bc77b18e
Sabahat <sabasabahat17@gmail.com>
2025-02-28 16:41:52 +0500

    after login render homepage successfully

--- add_user/admin.py
+++ add_user/admin.py
@@ -1,7 +1,30 @@
-from django.contrib import admin
-from .models import User
-from django.contrib.auth.decorators import login_required
+# from django.contrib import admin
+# from .models import User
+# from django.contrib.auth.decorators import login_required
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'password')
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'email', 'password')
+
+# ********************************new******************************
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('name', 'email', 'is_staff','password','is_superuser', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'email', 'password1', 'password2', 'is_staff', 'is_superuser')}
+        ),
+    )
+
+admin.site.register(User, CustomUserAdmin)
